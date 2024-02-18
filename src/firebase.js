a8dcccc361cf90b9995fde9d4ef2d627614f6cba
Karina <umiralikarina@gmail.com>
2024-02-19 00:21:06 +0600

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+//import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/database'
+import 'firebase/storage'
+import 'firebase/messaging'
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+});
+
+export const auth = app.auth()
+export default app
